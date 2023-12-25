f525a2a6889fa250f6b6add0e75ea3b4c458e701
Jovan Smiljanic <smiljanicjovan9@gmail.com>
2023-12-25 17:54:11 +0100

    Metadata added

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -18,6 +18,12 @@ const comfortaa = Comfortaa({
   subsets: ["latin"],
 });
 
+export const metadata = {
+  title: "Medisoft",
+  description:
+    "Medisoft: Streamlining Healthcare Management. Experience unparalleled efficiency with Medisoft, the leading medical clinic software. Designed for healthcare professionals, our intuitive platform simplifies patient scheduling, billing, and electronic health records management. With Medisoft, enjoy seamless integration, secure data handling, and customizable features tailored to your clinic's needs. Elevate patient care with Medisoft's cutting-edge technology, ensuring a more organized, efficient, and compliant medical practice. Ideal for small to mid-sized clinics looking for an affordable, reliable solution.",
+};
+
 export default function RootLayout({
   children,
   params,
@@ -40,8 +46,6 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width" />
 
         <title>Medisoft</title>
-
-        <link rel="icon" href="/logo.png" />
       </head>
 
       <body>
